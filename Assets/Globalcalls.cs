dc96be0e1f966c1449605e3203af44395a383873
MarvinKnobloch <NivramKnobloch@gmail.com>
2023-11-14 22:30:20 +0100

    savezone + barrier update

--- Assets/Globalcalls.cs
+++ Assets/Globalcalls.cs
@@ -9,7 +9,6 @@ public static class Globalcalls
     public static bool jumpcantriggerswitch;
 
     public static Vector3 playeresetpoint;
-    public static GameObject currentsavebutton;
 
     public static GameObject boundscolliderobj;
     public static int savecameradistance = 8;
--- Assets/Puzzles/Setresetpoint.cs
+++ Assets/Puzzles/Setresetpoint.cs
@@ -5,7 +5,6 @@ using TMPro;
 
 public class Setresetpoint : MonoBehaviour
 {
-    private BoxCollider2D boxCollider2D;
     private Vector3 resetposi;
 
     [SerializeField] private GameObject sectionobj;
@@ -13,13 +12,9 @@ public class Setresetpoint : MonoBehaviour
 
     private void Awake()
     {
-        boxCollider2D = GetComponent<BoxCollider2D>();
-        boxCollider2D.size = new Vector2(1, 6);
-        boxCollider2D.offset = new Vector2(0, 3.5f);
-
-        resetposi = new Vector3(transform.position.x, transform.position.y + 0.8f, transform.position.z);
         sectionobj = GetComponentInParent<Sectioncamera>().sectiongameobj;
         camdistance = GetComponentInParent<Sectioncamera>().cameradistance;
+        resetposi = transform.GetChild(0).transform.position;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -28,17 +23,6 @@ public class Setresetpoint : MonoBehaviour
             Globalcalls.playeresetpoint = resetposi;
             Globalcalls.boundscolliderobj = sectionobj;
             Globalcalls.savecameradistance = camdistance;
-            if(Globalcalls.currentsavebutton == null) gameObject.GetComponentInChildren<TextMeshProUGUI>().color = Color.black;
-            else
-            {
-                if (Globalcalls.currentsavebutton != gameObject)
-                {
-                    Globalcalls.currentsavebutton.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
-                    gameObject.GetComponentInChildren<TextMeshProUGUI>().color = Color.black;
-                }
-            }
-            Globalcalls.currentsavebutton = gameObject;
-        }
-            
+        }          
     }
 }
