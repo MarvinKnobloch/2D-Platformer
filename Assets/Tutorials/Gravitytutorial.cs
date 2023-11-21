92736a7b112d40118c274f1834940de0d5f1a9f2
MarvinKnobloch <NivramKnobloch@gmail.com>
2023-11-21 22:21:36 +0100

    level design rework

--- Assets/Tutorials/Gravitytutorial.cs
+++ Assets/Tutorials/Gravitytutorial.cs
@@ -29,9 +29,7 @@ public class Gravitytutorial : MonoBehaviour
     private void hotkeysandtextupdate()
     {
         gravityhotkey = controls.Player.Gravityswitch.GetBindingDisplayString();
-        tutorialtext.text = "Step on the \"S\" button to the save your postion.\n" +
-                            "Black zone = death zone and you will be ported back to your last save postion.\n" +
-                            "\nAfter stepping on the \"G\" press <color=green>" + gravityhotkey + "</color> to switch gravity.\n" +
+        tutorialtext.text = "\nAfter stepping on the \"G\" press <color=green>" + gravityhotkey + "</color> to switch gravity.\n" +
                             "The number on the bottom left will display how often you can use this ability.";
     }
 }
